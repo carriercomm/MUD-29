becac561288c270a60d42e890d378e58f5c627a9
MrZerg27 <bhaase97@yahoo.com>
2015-06-06 12:17:32 -0500

    Added Json stuff

---
+++ MUDHierarchy/Test/src/npcOutlineTest/npcTestMain.java
@@ -0,0 +1,18 @@
+package npcOutlineTest;
+import java.util.*;
+import org.json.*;
+import java.io.*;
+import org.json.simple.*;
+
+//import javax.json;
+
+public class npcTestMain {
+
+	public static void main(String[] args) {
+		 //JSONObject NpcTest = Json.createObjectBuilder().build();
+		JSONArray NpcTest = (JSONArray) parser.parse(new FileReader("c:\\exer4-courses.json"));
+		
+
+	}
+
+}
