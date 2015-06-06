33f035bd53d7fe9255b5f478956a56395a521757
MrZerg27 <bhaase97@yahoo.com>
2015-06-06 13:51:26 -0500

    Testing Json

--- MUDHierarchy/Test/src/npcOutlineTest/npcTestMain.java
+++ MUDHierarchy/Test/src/npcOutlineTest/npcTestMain.java
@@ -1,8 +1,15 @@
 package npcOutlineTest;
 import java.util.*;
-import org.json.*;
 import java.io.*;
-import org.json.simple.*;
+
+//import org.json.simple.*;
+//import org.json.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 //import javax.json;
 
@@ -10,7 +17,27 @@ public class npcTestMain {
 
 	public static void main(String[] args) {
 		 //JSONObject NpcTest = Json.createObjectBuilder().build();
-		JSONArray NpcTest = (JSONArray) parser.parse(new FileReader("c:\\exer4-courses.json"));
+		JSONParser parser = new JSONParser();
+		JSONValue test = new JSONValue();
+		
+		
+		try {
+			JSONArray NpcTest = new JSONArray();
+			NpcTest.add(parser.parse(new FileReader("D:/Users/Bennett/Documents/Workspace/MUDHierarchy/res/creatures/NpcOutline.json")));
+			JSONObject NpcClass =(JSONObject) NpcTest.get(0);
+			System.out.print(NpcClass.get("Name"));
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 
 	}
