a60a40d875dd964c56eb50708a6fe5a484722880
Colin Dietz <clndtz@gmail.com>
2015-06-12 02:01:45 -0500

    Edited Tokenizer

--- MUDHierarchy/src/parsers/Tokenizer.java
+++ MUDHierarchy/src/parsers/Tokenizer.java
@@ -1,15 +1,12 @@
 package parsers;
 
 import java.util.ArrayList;
-import java.io.*;
 
 // Tokenizes text -- Extracts chunks of text separated by spaces or tabs
 // also allows comments in text by ignoring everything in a line followed by the # sign
 public class Tokenizer
 {
 	private ArrayList<String> tokens = new ArrayList<String>();
-	private FileReader fr;
-	private BufferedReader br;
 	
 	private int currentToken = 0;
 	private boolean preserveCase;
@@ -19,25 +16,9 @@ public class Tokenizer
 		this.preserveCase = preserveCase;	//preserve text case
 	}
 	
-	public void addString(String input)
+	public void addTokens(String input)
 	{
 		generateTokens(sanatize(input));
-	}
-	
-	public void addFile(String fileName) throws Exception	//throws FileNotFound
-	{														//or FileIO
-		fr = new FileReader(fileName);
-		br = new BufferedReader(fr);
-		
-		String line = br.readLine();
-		while(line != null)
-		{
-			addString(line);
-			line = br.readLine();
-		}
-		
-		br.close();
-		fr.close();
 	}
 	
 	private String sanatize(String input)	// converts input to trimmed lower case with white spaces converged
@@ -54,63 +35,43 @@ public class Tokenizer
 		
 		int previousPos = 0;
 		int position = input.indexOf(" ", 0);
+		
 		for(int i = 0; i < size; i++)
 		{
 			String temp = input.substring(previousPos, position);
-			if(!temp.startsWith("#"))	// sorts out comments -- they fill all space in a line after a # sign
-			{
-				tokens.add(temp);	// grabs all of the words with spaces separating them
-				previousPos = position + 1;
+
+			tokens.add(temp);	// grabs all of the words with spaces separating them
+			previousPos = position + 1;
 			
-				if(i != (size - 2))
-					position = input.indexOf(" ", previousPos);	// if this isn't the second to last word
-				else
-					position = input.length();	// if it is the second to last word
-			}
+			if(i != (size - 2))
+				position = input.indexOf(" ", previousPos);	// if this isn't the second to last word
 			else
-			{
-				break;
-			}
+				position = input.length();	// if it is the second to last word
 		}
+		tokens.add("."); // terminator token
 	}
 	
-	public String getNext()	// grab one token
+	public String get()
 	{
-		String token = tokens.get(currentToken);
-		currentToken++;
-		return token;
+		return tokens.get(currentToken);
 	}
 	
-	public String[] getNext(int numTokens)	//next n tokens
+	public void next()	// grab one token
 	{
-		String[] temp = new String[numTokens];
-		for(int i = 0; i < numTokens; i++)
-		{
-			temp[i] = tokens.get(currentToken);
+		if (hasNext())
 			currentToken++;
-		}
-		return temp;
 	}
 	
-	public String[] getAll()	//all tokens. not just from here current to end
+	public String[] getAll()	//returns tokens list
 	{
-		String[] tempStrArray = (String[]) tokens.toArray();
-		currentToken = tokens.size();
-		return tempStrArray;
+		return (String[]) tokens.toArray();
 	}
 	
 	public boolean hasNext()
 	{
-		if(currentToken < tokens.size())
+		if((currentToken + 1) < tokens.size())
 			return true;
 		return false;
-	}
-	
-	public boolean hasNext(int numTokens)
-	{
-		if((tokens.size() - currentToken) < numTokens)
-			return false;
-		return true;
 	}
 	
 	public void previous()	//back track
@@ -119,18 +80,12 @@ public class Tokenizer
 			currentToken--;
 	}
 	
-	public void previous(int numTokens)
-	{
-		if((currentToken - numTokens) >= 0)
-			currentToken = (currentToken - numTokens);
-	}
-	
 	public void resetToken()	//reset current token
 	{
 		currentToken = 0;
 	}
 	
-	public void reset()
+	public void resetAll()
 	{
 		tokens = new ArrayList<String>();	//reset all
 		currentToken = 0;
