652d4d16237ef748debcdcc513870014460d80fb
Colin Dietz <clndtz@gmail.com>
2015-07-19 01:53:44 -0500

    Edited javascript

--- MUDHierarchy/tools/script.js
+++ MUDHierarchy/tools/script.js
@@ -32,13 +32,16 @@ function indexJsonCallback()
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
 	{
 		console.log("File Index XMLHttp content received");
-		fileIndex = JSON.parse(xmlhttp.responseText);
-		loadJson(fileIndex.FileTypes[0].Path, "pageJson");	// load page contents
+		document.getElementById("sidebar").innerHTML = '';	// clear the page contents
+		fileIndex = JSON.parse(xmlhttp.responseText);		// set the file Index vale for the page to use
+		loadJson(fileIndex.FileTypes[0].Path, "pageJson");	// load page contents .json
 		
-		document.getElementById("sidebar").innerHTML = '';
-		for(var member in fileIndex.FileTypes)
+		var title = fileIndex.FileTypes[0].Name	// get page title
+		document.getElementById("content").innerHTML += '<h2>' + title + '</h2>';	// set page title
+		
+		for(var member in fileIndex.FileTypes)	//  for each index file
 		{
-			var name = fileIndex.FileTypes[member].Name;
+			var name = fileIndex.FileTypes[member].Name;	// get its name and a make a button out of it
 			console.log("adding: " + name);
 			
 			var button = document.createElement("input");
@@ -46,8 +49,10 @@ function indexJsonCallback()
 			button.setAttribute("id", "index-button");
 			button.setAttribute("onclick", "buttonCallback(\"" + name + "\")");
 			button.setAttribute("value", name);
-			button.innerHTML = name;
-			document.getElementById("sidebar").appendChild(button);
+			document.getElementById("sidebar").appendChild(button);	// add the button to the sidebar
+			
+			var br = document.createElement("br");
+			document.getElementById("sidebar").appendChild(br);	// add a break after the button
 		}
 	}
 }
@@ -59,6 +64,7 @@ function pageJsonCallback()
 		console.log("XMLHttp content received");
 		var jsonObj = JSON.parse(xmlhttp.responseText);
 		document.getElementById("content").innerHTML = '';
+		
 		for(var member in jsonObj)
 		{
 			document.getElementById("content").innerHTML += '<p>' + member + ': ' + jsonObj[member] + '</p>';
