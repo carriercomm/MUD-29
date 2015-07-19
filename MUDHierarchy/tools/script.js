edea864fce7e8f7bd129ef369cacf57d7a7dd095
Colin Dietz <clndtz@gmail.com>
2015-07-18 20:35:16 -0500

    Edited javascript

--- MUDHierarchy/tools/script.js
+++ MUDHierarchy/tools/script.js
@@ -33,10 +33,10 @@ function indexJsonCallback()
 	{
 		console.log("File Index XMLHttp content received...");
 		fileIndex = JSON.parse(xmlhttp.responseText);
-		loadJson(fileIndex.FileTypes[0].Path, "pageJson");	// load page contents
+		loadJson(fileIndex.Path, "pageJson");	// load page contents
 		
 		document.getElementById("sidebar").innerHTML = '';
-		for(var member in fileIndex.FileTypes)
+		for(var member in fileIndex)
 		{
 			document.getElementById("sidebar").innerHTML += 
 			'<input id="index-button" type="button" value=' + member.Name + ' onclick="buttonCallback(' + member.Name + ');" />';
@@ -60,7 +60,7 @@ function pageJsonCallback()
 
 function buttonCallback(name)
 {
-	for(var member in fileIndex.FileTypes)
+	for(var member in fileIndex)
 	{
 		if(member.Name == name)
 		{
