e0a0f58d26d94309d3d5af72f7425d15ceeee57a
Colin Dietz <clndtz@gmail.com>
2015-07-18 20:06:41 -0500

    Edited javascript, css, html

---
+++ MUDHierarchy/tools/script.js
@@ -0,0 +1,76 @@
+var xmlhttp = new XMLHttpRequest();
+var fileIndex = null;
+
+window.onload = function()
+{
+	jsonIndex = loadJson(index.json, indexJson);
+	while(true)
+	{
+		if(fileIndex != null)
+		{
+			loadJson(fileIndex.FileTypes[0].Path, pageJson);
+			break;
+		}
+	}
+}
+
+function loadJson(path, processAs)
+{
+	switch(processAs)
+	{
+		case indexJson:
+			xmlhttp.onreadystatechange = indexJsonCallback;
+		break;
+		case pageJson:
+			xmlhttp.onreadystatechange = pageJsonCallback;
+		break;
+		default:
+			console.log("Error: undefined processing flag");
+		break;
+	}
+
+	xmlhttp.open("GET", path, true);
+	xmlhttp.overrideMimeType("application/json");
+	xmlhttp.send();
+	console.log(path + " XMLHttp requested...");
+}
+
+function indexJsonCallback()
+{
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+	{
+		console.log("File Index XMLHttp content received...");
+		fileIndex = JSON.parse(xmlhttp.responseText);
+		document.getElementById("sidebar").innerHTML = '';
+		for(var mamber in fileIndex.FileTypes)
+		{
+			document.getElementById("sidebar").innerHTML += 
+			'<input id="index-button" type="button" value=' + member.Name + ' onclick="buttonCallback(' + member.Name + ');" />;'
+		}
+	}
+}
+
+function pageJsonCallback()
+{
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+	{
+		console.log("XMLHttp content received...");
+		var jsonObj = JSON.parse(xmlhttp.responseText);
+		document.getElementById("content").innerHTML = '';
+		for(var member in jsonObj)
+		{
+			document.getElementById("content").innerHTML += '<p>' + member + ': ' + jsonObj[member] + '</p>';
+		}
+	}
+}
+
+function buttonCallback(name)
+{
+	for(var member in fileIndex.FileTypes)
+	{
+		if(member.Name == name)
+		{
+			loadJson(member.Path, pageJson);
+		}
+	}
+}
